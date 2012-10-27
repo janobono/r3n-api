35120f83511ab991678ab2324bc9926c0fccfd7d
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2012-10-27 10:20:08 +0000

    format changed

--- r3n/r3n-api/src/main/java/sk/r3n/properties/R3NProperties.java
+++ r3n/r3n-api/src/main/java/sk/r3n/properties/R3NProperties.java
@@ -1,9 +1,7 @@
 package sk.r3n.properties;
 
 public interface R3NProperties {
-
-    public String FILE_NAME = "sk.r3n.properties.R3NProperties.file_name";
-
+    
     public String get(String key, String defaultValue);
 
     public void set(String key, String value);
@@ -11,4 +9,5 @@ public interface R3NProperties {
     public String decrypt(String string);
 
     public String encrypt(String string);
+    
 }
