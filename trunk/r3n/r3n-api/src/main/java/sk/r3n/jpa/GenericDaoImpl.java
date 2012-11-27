a58ca23dc14887181764770bc125a397620e165e
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2012-11-27 20:57:06 +0000

    format

--- trunk/r3n/r3n-api/src/main/java/sk/r3n/jpa/GenericDaoImpl.java
+++ trunk/r3n/r3n-api/src/main/java/sk/r3n/jpa/GenericDaoImpl.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.persistence.TemporalType;
 import javax.persistence.TypedQuery;
 
