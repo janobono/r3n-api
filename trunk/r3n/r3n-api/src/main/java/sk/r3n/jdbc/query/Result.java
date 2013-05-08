d3c0df9ad7bdc753d249e89c6c8dd4518b1c2311
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2013-05-08 17:38:30 +0000

    result object changed

--- trunk/r3n/r3n-api/src/main/java/sk/r3n/jdbc/query/Result.java
+++ trunk/r3n/r3n-api/src/main/java/sk/r3n/jdbc/query/Result.java
@@ -1,83 +1,61 @@
 package sk.r3n.jdbc.query;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Result<T> implements Serializable {
 
-    private final List<T> results;
+    private int pageSize;
 
-    private final int totalPages;
+    private List<T> results;
 
-    private final int totalRowsCount;
+    private int totalRowsCount;
 
-    private Result(Builder builder) {
-        if (builder.results == null) {
-            throw new NullPointerException("Search result is null.");
-        }
+    public int getPageSize() {
+        return pageSize;
+    }
 
-        this.results = builder.results;
-        this.totalRowsCount = builder.totalRowsCount;
-
-        Integer numOfAllPages = builder.totalPages;
-
-        if (builder.pageSize != null) {
-            if (builder.totalRowsCount <= builder.pageSize && builder.totalRowsCount > 0) {
-                numOfAllPages = 1;
-            }
-            if (builder.totalRowsCount > builder.pageSize) {
-                int pageOfset = builder.totalRowsCount % builder.pageSize == 0 ? 0 : 1;
-                numOfAllPages = builder.totalRowsCount / builder.pageSize + pageOfset;
-            }
-        }
-
-        this.totalPages = numOfAllPages;
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
     }
 
     public List<T> getResults() {
+        if (results == null) {
+            results = new ArrayList<>();
+        }
         return results;
     }
 
-    public Integer getTotalPages() {
-        return totalPages;
+    public void setResults(List<T> results) {
+        this.results = results;
     }
 
-    public Integer getTotalRowsCount() {
+    public int getTotalRowsCount() {
         return totalRowsCount;
     }
 
-    public static class Builder<T> {
+    public void setTotalRowsCount(int totalRowsCount) {
+        this.totalRowsCount = totalRowsCount;
+    }
 
-        private List<T> results;
-
-        private Integer totalPages;
-
-        private Integer totalRowsCount;
-
-        private Integer pageSize;
-
-        public Builder setResults(List<T> results) {
-            this.results = results;
-            return this;
+    public int getTotalPages() {
+        if (totalRowsCount <= 0 || pageSize <= 0) {
+            return 0;
         }
-
-        public Builder setTotalPages(int totalPages) {
-            this.totalPages = totalPages;
-            return this;
+        if (totalRowsCount <= pageSize) {
+            return 1;
         }
-
-        public Builder setTotalRowsCount(int totalRowsCount) {
-            this.totalRowsCount = totalRowsCount;
-            return this;
+        int result = totalRowsCount / pageSize;
+        if (totalRowsCount % pageSize > 0) {
+            result++;
         }
+        return result;
+    }
 
-        public Builder setPageSize(int pageSize) {
-            this.pageSize = pageSize;
-            return this;
-        }
-
-        public Result build() {
-            return new Result(this);
-        }
+    @Override
+    public String toString() {
+        return "Result{" + "pageSize=" + pageSize + ", results=" + results
+                + ", totalRowsCount=" + totalRowsCount + '}';
     }
 }
