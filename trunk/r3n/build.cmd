84f1334de13712bb81003fe4704a8e5f3dfd03ab
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2013-08-12 18:36:56 +0000

    build cmd na novu verziu javy

--- trunk/r3n/build.cmd
+++ trunk/r3n/build.cmd
@@ -1,5 +1,5 @@
 @echo off
-SET JAVA_HOME="C:\Progra~1\Java\jdk1.7.0_21"
+SET JAVA_HOME="C:\Progra~1\Java\jdk1.7.0_25"
 SET MAVEN_HOME="C:\Progra~1\NetBeans 7.3.1\java\maven\bin"
 SET Path=%Path%;%JAVA_HOME%;%MAVEN_HOME%
 SET MYPATH=%CD% 
