bb4b4948e205747e6a599ef95d03f2bd9f2ef993
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2013-09-17 20:14:16 +0000

    build script

--- r3n/build.sh
+++ r3n/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-JAVA_HOME="$HOME/opt/jdk1.7.0_21/bin"
-MAVEN_HOME="$HOME/opt/netbeans-7.3.1/java/maven/bin"
-PATH=$JAVA_HOME:$MAVEN_HOME:$PATH
+JAVA_HOME="$HOME/opt/jdk1.7.0_21"
+MAVEN_HOME="$HOME/opt/netbeans-7.3.1/java/maven"
+PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH
 
 mvn -version
 mvn clean
