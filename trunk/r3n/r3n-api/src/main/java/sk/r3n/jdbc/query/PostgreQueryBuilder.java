36bb89d474dde275c690b2f7316c2e2bd8eb5964
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2013-11-02 16:33:27 +0000

    code refactoring

--- trunk/r3n/r3n-api/src/main/java/sk/r3n/jdbc/query/PostgreQueryBuilder.java
+++ trunk/r3n/r3n-api/src/main/java/sk/r3n/jdbc/query/PostgreQueryBuilder.java
@@ -1,22 +1,20 @@
 package sk.r3n.jdbc.query;
 
 import java.util.List;
-import static sk.r3n.jdbc.query.AbstractQueryBuilder.criteriaToWhere;
-import static sk.r3n.jdbc.query.OraQueryBuilder.criteriaToCountSQL;
-import static sk.r3n.jdbc.query.OraQueryBuilder.criteriaToSQL;
+import sk.r3n.jdbc.Sequence;
 
-public class PosQueryBuilder extends AbstractQueryBuilder {
+/**
+ *
+ * @author jan
+ */
+public class PostgreQueryBuilder extends AbstractQueryBuilder {
 
-    public static void criteriaToCountSQL(QueryAttribute[] resultColumns, String fromSQL,
+    @Override
+    public void criteriaToCountSQL(QueryAttribute[] resultColumns, String fromSQL, boolean distinct,
             QueryCriteria criteria, StringBuilder countSQL, List<Object> countParams) {
-        criteriaToCountSQL(resultColumns, fromSQL, false, criteria, countSQL, countParams);
-    }
-
-    public static void criteriaToCountSQL(QueryAttribute[] resultColumns, String fromSQL, boolean distinct,
-            QueryCriteria criteria, StringBuilder countSQL, List<Object> countParams) {
-        countSQL.append("select count(*) from (select ");
+        countSQL.append("SELECT COUNT(*) FROM (SELECT ");
         if (distinct) {
-            countSQL.append("distinct ");
+            countSQL.append("DISTINCT ");
         }
         for (int i = 0; i < resultColumns.length; i++) {
             QueryAttribute column = resultColumns[i];
@@ -25,20 +23,16 @@ public class PosQueryBuilder extends AbstractQueryBuilder {
                 countSQL.append(", ");
             }
         }
-        countSQL.append(" from ").append(fromSQL);
+        countSQL.append(" FROM ").append(fromSQL);
         if (criteria.isCriteria()) {
             countSQL.append(criteriaToWhere(criteria, countParams));
         }
         countSQL.append(") res");
     }
 
-    public static void criteriaToSQL(QueryAttribute[] resultColumns, String fromSQL,
-            QueryCriteria criteria, StringBuilder sql, List<Object> params) {
-        criteriaToSQL(resultColumns, fromSQL, false, criteria, sql, params);
-    }
-
-    public static void criteriaToSQL(QueryAttribute[] resultColumns, String fromSQL, boolean distinct,
-            QueryCriteria criteria, StringBuilder sql, List<Object> params) {
+    @Override
+    public void criteriaToSQL(QueryAttribute[] resultColumns, String fromSQL, boolean distinct, QueryCriteria criteria,
+            StringBuilder sql, List<Object> params) {
         String where = null;
         String orderBy = null;
         if (criteria.isCriteria()) {
@@ -48,9 +42,9 @@ public class PosQueryBuilder extends AbstractQueryBuilder {
             orderBy = criteriaToOrderBy(resultColumns, criteria);
         }
 
-        sql.append("select ");
+        sql.append("SELECT ");
         if (distinct) {
-            sql.append("distinct ");
+            sql.append("DISTINCT ");
         }
         for (int i = 0; i < resultColumns.length; i++) {
             sql.append(resultColumns[i].nameWithAlias());
@@ -58,15 +52,34 @@ public class PosQueryBuilder extends AbstractQueryBuilder {
                 sql.append(", ");
             }
         }
-        sql.append(" from ").append(fromSQL);
+        sql.append(" FROM ").append(fromSQL);
         if (where != null) {
             sql.append(where);
         }
         if (orderBy != null) {
             sql.append(orderBy);
         }
-        sql.append(" offset ? limit ?");
+        sql.append(" OFFSET ? LIMIT ?");
         params.add(criteria.getFirstRow());
         params.add(criteria.getPageSize());
     }
+
+    @Override
+    public String insert(QueryTable table, Sequence sequence, QueryAttribute[] attributes) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO ");
+        sb.append(table.name());
+        sb.append("(").append(getColumns(attributes, false, ", "));
+        sb.append(") VALUES (");
+        sb.append(sequence.nextval()).append(", ");
+        for (int i = 1; i < attributes.length; i++) {
+            sb.append("?");
+            if (i < attributes.length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append(") RETURNING ").append(attributes[0].name());
+        return sb.toString();
+    }
+
 }
