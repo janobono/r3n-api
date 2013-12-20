ce3815e6159f026dfeb3edb197875a3abc3c81fd
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2013-12-20 12:36:29 +0000

    tag 2.0.1

--- tags/2.0.1/r3n/r3n-api/src/main/java/sk/r3n/ui/IconType.java
+++
@@ -1,9 +0,0 @@
-package sk.r3n.ui;
-
-public enum IconType {
-
-    ENABLED,
-    DISABLED,
-    PRESSED;
-
-}
--- tags/2.0.1/r3n/r3n-api/src/main/java/sk/r3n/ui/UIActionExecutor.java
+++
@@ -1,7 +0,0 @@
-package sk.r3n.ui;
-
-public interface UIActionExecutor {
-
-    public void execute(UIActionKey actionKey, Object source);
-
-}
