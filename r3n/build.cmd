61a1dd4d0c0a910121b4bcdec6cdc673c3ddb57c
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2014-08-18 15:34:31 +0000

    new project structure

--- r3n/build.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-SET JAVA_HOME="C:\Progra~1\Java\jdk1.7.0_25"
-SET MAVEN_HOME="C:\Progra~1\NetBeans 7.3.1\java\maven\bin"
-SET Path=%Path%;%JAVA_HOME%;%MAVEN_HOME%
-SET MYPATH=%CD% 
-
-mvn clean install -DskipTests
