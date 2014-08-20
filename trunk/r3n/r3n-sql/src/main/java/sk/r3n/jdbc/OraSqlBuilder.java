71e7ece068b54a5085b7434c2b961a299d50a11c
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2014-08-20 20:27:26 +0000

    structure changed

--- trunk/r3n/r3n-sql/src/main/java/sk/r3n/jdbc/OraSqlBuilder.java
+++ trunk/r3n/r3n-sql/src/main/java/sk/r3n/jdbc/OraSqlBuilder.java
@@ -1,16 +1,21 @@
 package sk.r3n.jdbc;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
+import java.util.Date;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import sk.r3n.sql.DELETE;
-import sk.r3n.sql.INSERT;
-import sk.r3n.sql.SELECT;
+import sk.r3n.sql.Column;
+import sk.r3n.sql.Query;
 import sk.r3n.sql.Sequence;
-import sk.r3n.sql.UPDATE;
+import sk.r3n.util.FileUtil;
 
 public class OraSqlBuilder extends SqlBuilder {
 
@@ -54,5 +59,135 @@ public class OraSqlBuilder extends SqlBuilder {
         return result;
     }
 
-   
+    @Override
+    protected String toPaginatedSelect(Query query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected String toInsertReturningValue(Query query) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("BEGIN INSERT INTO ").append(query.getTable()).append(SPACE).append(LEFT_BRACE);
+        Column[] columns = query.getColumns();
+        for (int i = 0; i < columns.length; i++) {
+            sql.append(columns[i]);
+            if (i < columns.length - 1) {
+                sql.append(COMMA).append(SPACE);
+            }
+        }
+        sql.append(RIGHT_BRACE).append(" VALUES ").append(LEFT_BRACE);
+        Object[] values = query.getValues();
+        for (int i = 0; i < columns.length; i++) {
+            if (values[i] instanceof Sequence) {
+                sql.append(nextVal((Sequence) values[i]));
+            } else {
+                sql.append(QUESTION_MARK);
+                params().add(values[i]);
+            }
+            if (i < columns.length - 1) {
+                sql.append(COMMA).append(SPACE);
+            }
+        }
+        sql.append(RIGHT_BRACE).append(" RETURNING ").append(query.getReturning()).append(" INTO ?; END;");
+        return sql.toString();
+    }
+
+    @Override
+    protected Object executeInsert(Connection connection, String sql, Column[] columns, Column returning)
+            throws SQLException {
+        Object result = null;
+        CallableStatement callableStatement = null;
+        try {
+            int index = params().size() + 1;
+
+            callableStatement = connection.prepareCall(sql);
+            setParams(connection, callableStatement);
+
+            switch (returning.getDataType()) {
+                case BOOLEAN:
+                    callableStatement.registerOutParameter(index, Types.BOOLEAN);
+                    break;
+                case STRING:
+                    callableStatement.registerOutParameter(index, Types.VARCHAR);
+                    break;
+                case SHORT:
+                    callableStatement.registerOutParameter(index, Types.SMALLINT);
+                    break;
+                case INTEGER:
+                    callableStatement.registerOutParameter(index, Types.INTEGER);
+                    break;
+                case LONG:
+                    callableStatement.registerOutParameter(index, Types.BIGINT);
+                    break;
+                case BIG_DECIMAL:
+                    callableStatement.registerOutParameter(index, Types.NUMERIC);
+                    break;
+                case DATE:
+                    callableStatement.registerOutParameter(index, Types.DATE);
+                    break;
+                case TIME:
+                    callableStatement.registerOutParameter(index, Types.TIME);
+                    break;
+                case TIME_STAMP:
+                    callableStatement.registerOutParameter(index, Types.TIMESTAMP);
+                    break;
+                case BLOB:
+                    callableStatement.registerOutParameter(index, Types.BLOB);
+                    break;
+            }
+
+            callableStatement.execute();
+
+            switch (returning.getDataType()) {
+                case BOOLEAN:
+                    result = callableStatement.getBoolean(index);
+                    break;
+                case STRING:
+                    result = callableStatement.getString(index);
+                    break;
+                case SHORT:
+                    result = callableStatement.getShort(index);
+                    break;
+                case INTEGER:
+                    result = callableStatement.getInt(index);
+                    break;
+                case LONG:
+                    result = callableStatement.getLong(index);
+                    break;
+                case BIG_DECIMAL:
+                    result = callableStatement.getBigDecimal(index);
+                    break;
+                case DATE:
+                    java.sql.Date date = callableStatement.getDate(index);
+                    result = new Date(date.getTime());
+                    break;
+                case TIME:
+                    java.sql.Time time = callableStatement.getTime(index);
+                    result = new Date(time.getTime());
+                    break;
+                case TIME_STAMP:
+                    java.sql.Timestamp timestamp = callableStatement.getTimestamp(index);
+                    result = new Date(timestamp.getTime());
+                    break;
+                case BLOB:
+                    File file = null;
+                    try {
+                        file = File.createTempFile("SQL", ".BIN", getTmpDir());
+                        Blob blob = callableStatement.getBlob(index);
+                        FileUtil.streamToFile(blob.getBinaryStream(1, blob.length()), file);
+                        result = file;
+                    } catch (IOException e) {
+                        if (file != null) {
+                            file.delete();
+                        }
+                        throw new SQLException(e);
+                    }
+                    break;
+            }
+        } finally {
+            SqlUtil.close(callableStatement);
+        }
+        return result;
+    }
+
 }
--- trunk/r3n/r3n-sql/src/main/java/sk/r3n/jdbc/PostgreSqlBuilder.java
+++ trunk/r3n/r3n-sql/src/main/java/sk/r3n/jdbc/PostgreSqlBuilder.java
@@ -1,11 +1,14 @@
 package sk.r3n.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import sk.r3n.sql.Column;
+import sk.r3n.sql.Query;
 import sk.r3n.sql.Sequence;
 
 public class PostgreSqlBuilder extends SqlBuilder {
@@ -44,6 +47,59 @@ public class PostgreSqlBuilder extends SqlBuilder {
 
         if (LOG.isDebugEnabled()) {
             LOG.debug("RESULT:" + Long.toString(result));
+        }
+        return result;
+    }
+
+    @Override
+    protected String toPaginatedSelect(Query query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected String toInsertReturningValue(Query query) {
+        StringBuilder sql = new StringBuilder();
+
+        sql.append("INSERT INTO ").append(query.getTable()).append(SPACE).append(LEFT_BRACE);
+        Column[] columns = query.getColumns();
+        for (int i = 0; i < columns.length; i++) {
+            sql.append(columns[i]);
+            if (i < columns.length - 1) {
+                sql.append(COMMA).append(SPACE);
+            }
+        }
+        sql.append(RIGHT_BRACE).append(" VALUES ").append(LEFT_BRACE);
+        Object[] values = query.getValues();
+        for (int i = 0; i < columns.length; i++) {
+            if (values[i] instanceof Sequence) {
+                sql.append(nextVal((Sequence) values[i]));
+            } else {
+                sql.append(QUESTION_MARK);
+                params().add(values[i]);
+            }
+            if (i < columns.length - 1) {
+                sql.append(COMMA).append(SPACE);
+            }
+        }
+        sql.append(RIGHT_BRACE).append(" RETURNING ").append(query.getReturning());
+        return sql.toString();
+    }
+
+    @Override
+    protected Object executeInsert(Connection connection, String sql, Column[] columns, Column returning) throws SQLException {
+        Object result;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            setParams(connection, preparedStatement);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            result = getColumn(resultSet, params().size() + 1, returning);
+        } finally {
+            SqlUtil.close(resultSet);
+            SqlUtil.close(preparedStatement);
         }
         return result;
     }
