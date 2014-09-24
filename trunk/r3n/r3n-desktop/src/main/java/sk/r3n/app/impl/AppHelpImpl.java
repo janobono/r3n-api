961b84a8f411fe058b5f236297978cf10596816e
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2014-09-24 15:40:38 +0000

    app help

--- trunk/r3n/r3n-desktop/src/main/java/sk/r3n/app/impl/AppHelpImpl.java
+++ trunk/r3n/r3n-desktop/src/main/java/sk/r3n/app/impl/AppHelpImpl.java
@@ -1,15 +1,15 @@
 package sk.r3n.app.impl;
 
-import java.awt.Desktop;
 import java.io.*;
-import java.net.URI;
+import java.net.URL;
 import java.text.MessageFormat;
 import java.util.Properties;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import sk.r3n.app.AppCore;
 import sk.r3n.app.AppHelp;
+import sk.r3n.app.AppProperties;
 import sk.r3n.app.AppProperty;
 
 public class AppHelpImpl implements AppHelp {
@@ -18,8 +18,7 @@ public class AppHelpImpl implements AppHelp {
 
         LOAD,
         SAVE,
-        SHOW_PAGE_ERR,
-        SHOW_PAGE_WARNING;
+        SHOW_PAGE_ERR;
 
         public String value() {
             return ResourceBundle.getBundle(AppHelpImpl.class.getCanonicalName()).getString(name());
@@ -30,7 +29,7 @@ public class AppHelpImpl implements AppHelp {
         }
     }
 
-    private static final Logger LOGGER = Logger.getLogger(AppHelp.class.getCanonicalName());
+    private static final Log LOG = LogFactory.getLog(AppHelp.class.getCanonicalName());
 
     private String DIR;
 
@@ -38,13 +37,23 @@ public class AppHelpImpl implements AppHelp {
 
     public void start() {
         properties = new Properties();
-        DIR = AppCore.getProperty(AppProperty.HELP_DIR.code());
+        if (AppCore.getModule(AppProperties.class.getCanonicalName()) != null) {
+            DIR = ((AppProperties) AppCore.getModule(AppProperties.class.getCanonicalName())).get(AppProperty.HELP_DIR);
+        } else {
+            DIR = AppCore.getProperty(AppProperty.HELP_DIR.code());
+        }
         InputStream in = null;
         try {
-            in = new FileInputStream(DIR + AppCore.getProperty(AppProperty.HELP_MAP.code()));
+            String mapFile;
+            if (AppCore.getModule(AppProperties.class.getCanonicalName()) != null) {
+                mapFile = ((AppProperties) AppCore.getModule(AppProperties.class.getCanonicalName())).get(AppProperty.HELP_MAP);
+            } else {
+                mapFile = AppCore.getProperty(AppProperty.HELP_MAP.code());
+            }
+            in = new FileInputStream(DIR + mapFile);
             properties.load(in);
         } catch (IOException e) {
-            LOGGER.log(Level.WARNING, HelpBundle.LOAD.value(), e);
+            LOG.error(HelpBundle.LOAD.value(), e);
         } finally {
             try {
                 if (in != null) {
@@ -72,19 +81,13 @@ public class AppHelpImpl implements AppHelp {
         }
     }
 
-    private void showPage(String name) {
-        if (Desktop.isDesktopSupported()) {
-            Desktop desktop = Desktop.getDesktop();
-            if (desktop.isSupported(Desktop.Action.BROWSE)) {
-                try {
-                    URI uri = new File(DIR + name).toURI();
-                    desktop.browse(uri);
-                    return;
-                } catch (IOException e) {
-                    LOGGER.log(Level.WARNING, HelpBundle.SHOW_PAGE_ERR.value(), e);
-                }
-            }
+    protected void showPage(String name) {
+        try {
+            URL url = new File(DIR + name).toURI().toURL();
+            new ProcessBuilder("x-www-browser", url.toString()).start();
+        } catch (Exception e) {
+            LOG.error(HelpBundle.SHOW_PAGE_ERR.value(), e);
         }
-        LOGGER.log(Level.WARNING, HelpBundle.SHOW_PAGE_WARNING.value(new Object[]{DIR, name}));
     }
+
 }
