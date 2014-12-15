4826461149a371360c2d877f568e6dc90b402bf1
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2014-12-15 12:28:11 +0000

    function object redefined

--- tags/3.0.0/r3n/r3n-sql/src/main/java/sk/r3n/sql/Function.java
+++ tags/3.0.0/r3n/r3n-sql/src/main/java/sk/r3n/sql/Function.java
@@ -4,24 +4,16 @@ import java.text.MessageFormat;
 
 public class Function extends Column {
 
-    private String function;
+    private final Object[] members;
 
-    public Function(String function, Column column) {
-        super(column.getName(), column.getTable(), column.getDataType());
-        this.function = function;
-    }
-
-    public String getFunction() {
-        return function;
-    }
-
-    public void setFunction(String function) {
-        this.function = function;
+    public Function(String function, DataType dataType, String... members) {
+        super(function, null, dataType);
+        this.members = members;
     }
 
     @Override
     public String nameWithAlias() {
-        return MessageFormat.format(function, super.nameWithAlias());
+        return MessageFormat.format(getName(), members);
     }
 
 }
