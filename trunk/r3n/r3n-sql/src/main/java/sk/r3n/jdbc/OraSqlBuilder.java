97db4ed3cc3725dce461ce0ee09585e019b00709
janobono@gmail.com <janobono@gmail.com@b57c1a30-44ef-00df-7576-628cc255dc0c>
2015-01-14 10:39:54 +0000

    column in insert and update like value

--- trunk/r3n/r3n-sql/src/main/java/sk/r3n/jdbc/OraSqlBuilder.java
+++ trunk/r3n/r3n-sql/src/main/java/sk/r3n/jdbc/OraSqlBuilder.java
@@ -14,7 +14,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import sk.r3n.sql.Column;
 import sk.r3n.sql.ColumnSelect;
-import sk.r3n.sql.Criterion;
 import sk.r3n.sql.DataType;
 import sk.r3n.sql.Join;
 import sk.r3n.sql.JoinCriterion;
@@ -243,8 +242,8 @@ public class OraSqlBuilder extends SqlBuilder {
         for (int i = 0; i < columns.length; i++) {
             if (values[i] instanceof Sequence) {
                 sql.append(nextVal((Sequence) values[i]));
-            } else if (values[i] instanceof Criterion) {
-                sql.append(((Criterion) values[i]).getRepresentation());
+            } else if (values[i] instanceof Column) {
+                sql.append((Column) values[i]);
             } else {
                 sql.append(QUESTION_MARK);
                 params().add(new SqlParam(columns[i].getDataType(), values[i]));
