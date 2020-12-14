87e358f5e2209582eb2a8a4cf9ff2c6654e610ff
janobono <janobono@gmail.com>
2020-12-14 09:02:13 +0100

    dependencies fixed
    oracle jdbc is in public maven repository
    test containers and new junit
    
    Signed-off-by: janobono <janobono@gmail.com>

--- oracle/mvn_oracle.cmd
+++
@@ -1,4 +0,0 @@
-echo off
-
-call mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=12.1.0.1 -Dpackaging=jar
-call mvn install:install-file -Dfile=ojdbc8.jar -DgroupId=com.oracle -DartifactId=ojdbc8 -Dversion=12.2.0.1 -Dpackaging=jar
--- oracle/mvn_oracle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mvn install:install-file -Dfile=ojdbc6.jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=12.1.0.1 -Dpackaging=jar
-mvn install:install-file -Dfile=ojdbc8.jar -DgroupId=com.oracle -DartifactId=ojdbc8 -Dversion=12.2.0.1 -Dpackaging=jar
